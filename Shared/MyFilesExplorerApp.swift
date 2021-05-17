925864f5f72bb454cc6cb94fc415b13519f443a5
Russell Archer <russell.archer@mac.com>
2021-05-17 13:29:50 +0100

    Initial Commit

---
+++ Shared/MyFilesExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFilesExplorerApp.swift
+//  Shared
+//
+//  Created by Russell Archer on 17/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFilesExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
